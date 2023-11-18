0e4d8297c3dacddb214e8f65fb744b8f187e42db
Ismael Bernad Tello <bernad.teism20@zaragoza.salesianos.edu>
2023-11-18 22:10:48 +0100

    Directorios U3A2 Creados
    Creado los directorios para el módulo original de la actividad 2 de la unidad 3

---
+++ U3 Organización y consulta de la información/U3A2 Creación de un módulo original/modulo_original/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "modulo_original",
+    'summary': """Modulo original para la tarea U3A2""",
+    'description': """
+        Modulo original para la tarea U3A2
+        
+    """,
+    'author': "ismael bernad tello",
+    'website': "http://www.salesianos.edu",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Tarea U3A2',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views.xml',
+    ],
+}
