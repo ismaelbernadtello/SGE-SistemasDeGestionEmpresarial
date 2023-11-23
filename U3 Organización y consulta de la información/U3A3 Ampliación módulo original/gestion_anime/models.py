09342e813c37500307676cea37bee98ff90cbae1
Ismael Bernad Tello <bernad.teism20@zaragoza.salesianos.edu>
2023-11-23 22:52:21 +0100

    U3A3 Herencia correcta

--- U3 Organización y consulta de la información/U3A3 Ampliación módulo original/gestion_anime/models.py
+++ U3 Organización y consulta de la información/U3A3 Ampliación módulo original/gestion_anime/models.py
@@ -4,12 +4,18 @@ from odoo import api, models, fields
 class Anime(models.Model):
     _name = 'gestion_anime.anime'
     _description = 'Clase para gestionar animes'
-
-    nombre = fields.Char(string='Título del Anime', required=True)
+    _rec_name = 'nombre'
+    
+    nombre = fields.Char(string='Nombre del Anime', required=True)
     genero = fields.Char(string='Género')
     demografia = fields.Char(string='Demografía')
     lanzamiento = fields.Integer(string='Año de Lanzamiento')
+    
+    #Tengo las 2 relaciones para poder probar a crear episodios con link y sin link para ver
+    #Funcionalmente no tiene sentido, pero es para probar la herencia
+    #De normal solo se usaria la relacion verepisodios_ids porque es la clase que hereda de episodio
     episodios_ids = fields.One2many('gestion_anime.episodio', 'anime_id', string='Episodios')
+    verepisodio_ids = fields.One2many('gestion_anime.ver_episodio', 'anime_id', string='Ver Episodios')
     
     # Calcula el total de episodios que tiene el anime
     total_episodios = fields.Integer(compute='_compute_total_episodios', store=True, string='Total de Episodios')
@@ -30,17 +36,17 @@ class Anime(models.Model):
                 anime.media_puntuacion = media_puntuacion
             else:
                 anime.media_puntuacion = 0.0
-            
+
 class Episodio(models.Model):
     _name = 'gestion_anime.episodio'
     _description = 'Clase para gestionar los episodios del anime'
 
+    id = fields.Integer(string='Id del Episodio', required=True, readonly=True, default=lambda self: self._get_next_id())
+    anime_id = fields.Many2one('gestion_anime.anime', string='Anime al que pertenece')
     titulo = fields.Char(string='Título del Episodio', required=True)
     duracion = fields.Float(string='Duración (min)')
     lanzamiento = fields.Date(string='Fecha de Lanzamiento')
     puntuacion = fields.Float(string='Puntuación')
-    anime_id = fields.Many2one('gestion_anime.anime', string='Anime al que pertenece')
-    
     
     
 class VerEpisodio(models.Model):
@@ -48,5 +54,6 @@ class VerEpisodio(models.Model):
     _description = 'Clase extendida para ver los episodios y encontrar su link'
     _inherit = 'gestion_anime.episodio'
 
+    anime_id = fields.Many2one('gestion_anime.anime', string='Anime al que pertenece')
     link = fields.Char(string='Enlace para Ver el Episodio')
     idioma = fields.Char(string='Idioma del Episodio')
